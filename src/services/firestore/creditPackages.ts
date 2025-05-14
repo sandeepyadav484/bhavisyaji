7c100f471657b4937a40b32da1944cf2a2ca85f2
Sandeep Yadav <sandeepya484@gmail.com>
2025-05-14 22:03:22 +0530

    fix: update prices for credit packages (Micro: 49, Standard: 99, Value: 499)

--- src/services/firestore/creditPackages.ts
+++ src/services/firestore/creditPackages.ts
@@ -4,7 +4,7 @@ export const creditPackages: CreditPackage[] = [
   {
     id: 'micro-pack',
     name: 'Micro Pack',
-    price: 99,
+    price: 49,
     credits: 10,
     description: 'Perfect for trying out our services',
     paymentLink: 'https://rzp.io/rzp/lyrWd9VS'
@@ -12,7 +12,7 @@ export const creditPackages: CreditPackage[] = [
   {
     id: 'standard-pack',
     name: 'Standard Pack',
-    price: 499,
+    price: 99,
     credits: 60,
     description: 'Most popular choice for regular users',
     paymentLink: 'https://rzp.io/rzp/5fNpWpos'
@@ -20,7 +20,7 @@ export const creditPackages: CreditPackage[] = [
   {
     id: 'value-pack',
     name: 'Value Pack',
-    price: 999,
+    price: 499,
     credits: 150,
     description: 'Best value for frequent users',
     paymentLink: 'https://rzp.io/rzp/RGpR3z9z'
